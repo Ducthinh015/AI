b88d263e38967e6dbfea84ab244443f3506ab714
THINH <158245952+Ducthinh015@users.noreply.github.com>
2025-05-07 13:45:21 +0700

    Update urls.py

--- game/urls.py
+++ game/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from . import views
-from .views import AIMoveAPIView
 
 urlpatterns = [
     path('init/', views.init_board, name='init_board'),
     path('moves/', views.get_moves, name='get_moves'),
     path('move/', views.make_move, name='make_move'),
-    path('games/<int:pk>/ai-move/', views.AIMoveAPIView.as_view(), name='ai-move')
-
+    path('games/<int:pk>/ai-move/', views.AIMoveAPIView.as_view(), name='ai-move'),
 ]
