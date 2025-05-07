a0d485d5dc9a2b70b956b2d67a629bc5af4cd552
THINH <158245952+Ducthinh015@users.noreply.github.com>
2025-05-07 13:44:31 +0700

    Update settings.py

--- draughts_backend/settings.py
+++ draughts_backend/settings.py
@@ -8,62 +8,41 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']  # Có thể giới hạn sau khi deploy
 
 # Ứng dụng cài đặt
-INSTALLED_APPS = [
-    'corsheaders',  # CORS nên nằm trên cùng
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'game',
-]
+ INSTALLED_APPS = [
+-    'corsheaders',  # CORS nên nằm trên cùng
++    'corsheaders',        # CORS nên nằm đầu
+     'django.contrib.auth',
+     'django.contrib.contenttypes',
+     'django.contrib.sessions',
+     'django.contrib.messages',
+     'django.contrib.staticfiles',
+     'rest_framework',
+     'game',
+ ]
 
-MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',  # Nên nằm đầu danh sách middleware
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-]
+ MIDDLEWARE = [
+     'corsheaders.middleware.CorsMiddleware',  # Nên nằm đầu
+     'django.middleware.common.CommonMiddleware',
+     'django.contrib.sessions.middleware.SessionMiddleware',
+     'django.middleware.csrf.CsrfViewMiddleware',
+     'django.contrib.auth.middleware.AuthenticationMiddleware',
+     'django.contrib.messages.middleware.MessageMiddleware',
+ ]
 
-# Cho phép tất cả origin gọi API (dùng trong giai đoạn dev)
-CORS_ALLOW_ALL_ORIGINS = True
+ # Cho phép tất cả origin gọi API (chỉ dev)
+ CORS_ALLOW_ALL_ORIGINS = True
++CORS_ALLOW_CREDENTIALS = True
+ CORS_ALLOW_HEADERS = list(default_headers) + [
+     # nếu cần thêm header
+ ]
 
-ROOT_URLCONF = 'draughts_backend.urls'
+ REST_FRAMEWORK = {
+     'DEFAULT_RENDERER_CLASSES': (
+         'rest_framework.renderers.JSONRenderer',
+         'rest_framework.renderers.BrowsableAPIRenderer',
+     ),
++    # Tắt authentication/permission mặc định để frontend gọi dễ
++    'DEFAULT_AUTHENTICATION_CLASSES': [],
++    'DEFAULT_PERMISSION_CLASSES': [],
+ }
 
-# Nếu không dùng template, bạn có thể để trống hoặc bỏ
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'draughts_backend.wsgi.application'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-STATIC_URL = '/static/'
-
-REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
-    ),
-}
