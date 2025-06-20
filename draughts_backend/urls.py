ab02414768fa9cccdf356f557d9e7a2cabc2dee2
THINH <158245952+Ducthinh015@users.noreply.github.com>
2025-06-20 23:52:10 +0700

    Delete draughts_backend directory

--- draughts_backend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # Tất cả API của game sẽ nằm dưới /api/game/
-    path('api/game/', include('game.urls')),
-]
